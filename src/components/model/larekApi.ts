467b162a97b0611a73b5788113e8ea86433ed445
Igor Rybolovlev <rikkaaar@gmail.com>
2025-12-10 21:43:50 +0300

    Api

--- src/components/model/larekApi.ts
+++ src/components/model/larekApi.ts
@@ -1,10 +1,50 @@
-import { Api } from '../base/api';
+import { Api, ApiListResponse } from '../base/api';
+import {
+	IProduct,
+	ILarekApi,
+	IOrderRequest,
+	IOrderResult,
+} from '@/types/components/model/larekApi';
 
-export class LarekApi extends Api {
-    readonly cdn: string;
+/**
+ * Класс для работы с API Larek
+ */
+export class LarekApi extends Api implements ILarekApi {
+	readonly cdn: string;
 
-    constructor(cdn: string, baseUrl: string, options?: RequestInit) {
-        super(baseUrl, options);
-        this.cdn = cdn;
-    }
+	constructor(cdn: string, baseUrl: string, options?: RequestInit) {
+		super(baseUrl, options);
+		this.cdn = cdn;
+	}
+
+	/**
+	 * Получить список продуктов
+	 */
+	async getProducts(): Promise<IProduct[]> {
+		const data = (await this.get('/product/')) as ApiListResponse<IProduct>;
+		return data.items.map((item) => ({
+			...item,
+			image: this.cdn + item.image,
+		}));
+	}
+
+	/**
+	 * Получить продукт по id
+	 * @param id - идентификатор продукта
+	 */
+	async getProduct(id: string): Promise<IProduct> {
+		const item = (await this.get(`/product/${id}`)) as IProduct;
+		return {
+			...item,
+			image: this.cdn + item.image,
+		};
+	}
+
+	/**
+	 * Создать заказ
+	 * @param order - данные заказа
+	 */
+	async createOrder(order: IOrderRequest): Promise<IOrderResult> {
+		return (await this.post('/order', order)) as IOrderResult;
+	}
 }
--- src/types/components/model/larekApi.ts
+++ src/types/components/model/larekApi.ts
@@ -1,1 +1,71 @@
-// Типы, поставляемые из API (api.yml)
+// Типы, поставляемые из API
+// Источник: https://larek-api.nomoreparties.co/weblarek.postman.json
+
+/**
+ * Категория продукта
+ */
+export type ProductCategory =
+	| 'софт-скил'
+	| 'хард-скил'
+	| 'другое'
+	| 'дополнительное'
+	| 'кнопка';
+
+/**
+ * Способ оплаты
+ */
+export type PaymentMethod = 'online' | 'cash';
+
+/**
+ * Продукт
+ */
+export interface IProduct {
+	id: string;
+	description: string;
+	image: string;
+	title: string;
+	category: ProductCategory;
+	price: number | null;
+}
+
+/**
+ * Запрос на создание заказа
+ */
+export interface IOrderRequest {
+	payment: PaymentMethod;
+	email: string;
+	phone: string;
+	address: string;
+	total: number;
+	items: string[];
+}
+
+/**
+ * Ответ после создания заказа
+ */
+export interface IOrderResult {
+	id: string;
+	total: number;
+}
+
+/**
+ * Интерфейс API для работы с Larek
+ */
+export interface ILarekApi {
+	/**
+	 * Получить список продуктов
+	 */
+	getProducts(): Promise<IProduct[]>;
+
+	/**
+	 * Получить продукт по id
+	 * @param id - идентификатор продукта
+	 */
+	getProduct(id: string): Promise<IProduct>;
+
+	/**
+	 * Создать заказ
+	 * @param order - данные заказа
+	 */
+	createOrder(order: IOrderRequest): Promise<IOrderResult>;
+}
